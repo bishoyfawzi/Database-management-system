de303b2bb1b5d41c932a5ce6ef07d14b75ad2e62
Your Name <you@example.com>
2019-11-25 03:12:10 +0200

    add function for create dir

--- db.sh
+++ db.sh
@@ -1,140 +1,85 @@
 #!/bin/bash
 #This script is written by Bishoy Fawzi
 #This script can add,remove databases , add,remove table , add,remove fields , add,remove record 
-#Please us this script as root
 
 
-read -s -p "Enter MYSQL root password: " rootpasswd
-until mysql -u root -p$rootpasswd  -e ";" ; do
-       read -s -p "Can't connect, please retry: " rootpasswd
-done
-
+mkdir "$HOME/DB-managemnt" 2>/dev/null
 echo "select the operation ************"
-echo "  1)Create New Database And New Username "
-echo "  2)Delete Table on Database " 
-echo "  3)Delete Database "
-echo "  4)Create Table on Database "
-echo "  5)Drop Field from table "
-echo "  6)ADD  Field to table "
+echo "  1)Create  Database "
+echo "  2)Delete Database " 
+echo "  3)Create Table "
+echo "  4)Delete Table  "
+echo "  5)Add Field from table "
+echo "  6)Delete  Field to table "
 echo "  7)Add records"
 echo "  8)Delete records"
 echo "  9)Select records with condition"
 read n
 case $n in
-  1) 	echo "Please enter the NAME of the new MySQL database! (example: database1)"
+  1) 	echo "Please enter the NAME of the db! (example: database1)"
 	read dbname
-	echo "Please enter the MySQL database CHARACTER SET! (example: latin1, utf8, ...)"
-	read charset
-	echo "Creating new MySQL database..."
-	mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;" 2>/dev/null
-	echo "Database successfully created!"
-	echo "Showing existing databases..."
-	mysql -uroot -p${rootpasswd} -e "show databases;" 2>/dev/null
-	echo ""
-	echo "Please enter the NAME of the new MySQL database user! (example: user1)"
-	read username
-	echo "Please enter the PASSWORD for the new MySQL database user!"
-	echo "Note: password will be hidden when typing"
-	read -s userpass
-	echo "Creating new user..."
-	mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';" 2>/dev/null
-	echo "User successfully created!"
-	echo ""
-	echo "Granting ALL privileges on ${dbname} to ${username}!" 
-	mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${dbname}.* TO '${username}'@'localhost';" 2>/dev/null
-	mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;" 2>/dev/null
-	echo "The Database ${dbname} And The Username ${username} has been created";;
+	mkdir "$HOME/DB-managemnt/${dbname}" 2>/dev/null
+	ls  $HOME/DB-managemnt/
+        echo " The DB ${dbname} is created ";;
 
-  2)    echo "Showing existing databases..."
-        mysql -uroot -p${rootpasswd} -e "show databases;" 2>/dev/null
+  2)    ls  $HOME/DB-managemnt/
         echo ""
-        echo "Please enter the NAME of MySQL database You Want To Delete a Table ! "
-        read dbdeltable
-	mysql -uroot -p${rootpasswd} ${dbdeltable} -e  "show tables;" 2>/dev/null
-	echo ""
-        echo "Showing exiting tables..."
-        mysql -uroot -p${rootpasswd} -e "show tables;" 2>/dev/null
-	echo "Please enter the NAME of table You Want To Delete ! "
-	read tablename
-	mysql -uroot -p${rootpasswd} -e "DROP TABLE ${dbdeltable}.${tablename}" 
-	mysql -uroot -p${rootpasswd} ${dbdeltable} -e  "show tables;" 2>/dev/null
-        echo ""
-	echo " The table ${tablename} is deleted";;
+        echo "Please enter the NAME of  database You Want To Delete a Table ! "
+        read dbname
+	rm -rf $HOME/DB-managemnt/${dbname}
+	echo " The DB ${dbname} is deleted"
+        ls  $HOME/DB-managemnt/ ;;
 
-  3)    echo "Showing existing databases..."
-        mysql -uroot -p${rootpasswd} -e "show databases;"
-	echo ""
-	echo "Please enter the NAME of MySQL database You Want To Delete! "
-        read dbdel
-	mysql -uroot -p${rootpasswd} -e "DROP DATABASE ${dbdel};"
-	mysql -uroot -p${rootpasswd} -e "show databases;"
-        echo ""
-	echo "Database ${dbdel} successfully Deleted!"
-
+  3)    ls  $HOME/DB-managemnt/
+	echo "Please enter the NAME database you want to create table on it ! "
+	read db
+	echo "Please enter the NAME table ! "
+	read tb
+	touch $HOME/DB-managemnt/${db}/${tb}.csv
+	echo " The table ${tb} is created"
+	ls  $HOME/DB-managemnt/${db}/
 	;;
-  4) echo "Showing existing databases..."
-        mysql -uroot -p${rootpasswd} -e "show databases;"
-	echo ""
-        echo "Please enter the NAME database you want to create table on it ! "
-        read dbcreate
+
+  4)    ls  $HOME/DB-managemnt/
+        echo "Please enter the NAME database you want to delete table on it ! "
+        read db
+	echo "--------------"
+	ls  $HOME/DB-managemnt/${db}/
         echo "Please enter the NAME table ! "
-	read tname
-	echo "Please enter the colums name and datatype excatly like this 'nameofc1 int,nameofc2 varchar(255),nameofc3 varchar(255),nameofc4 varchar(255)' ! "
-	read colums
-	mysql -uroot -p${rootpasswd} -e "CREATE TABLE ${dbcreate}.${tname} (${colums})"
-	mysql -uroot -p${rootpasswd} -e "DESCRIBE ${dbcreate}.${tname}"
-        echo "table  ${tname} successfully Created!"
+        read tb
+        rm $HOME/DB-managemnt/${db}/${tb}
+        echo " The table ${tb} is Deleted"
+        ls  $HOME/DB-managemnt/${db}/ 
 	;;
 
-  5)    echo "Showing existing databases..."
-        mysql -uroot -p${rootpasswd} -e "show databases;"
-        echo ""
-        echo "Please enter the NAME database you want to delete field table on it ! "
-        read dbfield
-        mysql -uroot -p${rootpasswd} ${dbfield} -e  "show tables;" 2>/dev/null
-        echo ""
-        echo "Please enter the NAME of table  ! "
-        read tfeild
-	mysql -uroot -p${rootpasswd} -e "DESCRIBE ${dbfield}.${tfeild}"
-	echo ""
-	echo "Please enter the NAME of feild  ! "
-        read feild
-	mysql -uroot -p${rootpasswd} -e "ALTER TABLE ${dbfield}.${tfeild} DROP ${feild} ;" 2>/dev/null
-        mysql -uroot -p${rootpasswd} -e "DESCRIBE ${dbfield}.${tfeild}" 2>/dev/null
-        echo "Field  ${feild} successfully Deleted!"	;;
-  6) echo "Showing existing databases..."
-        mysql -uroot -p${rootpasswd} -e "show databases;"
-        echo ""
-        echo "Please enter the NAME database you want to create field table on it ! "
-        read db6
-	mysql -uroot -p${rootpasswd} ${db6} -e  "show tables;" 2>/dev/null
-        echo ""
-        echo "Please enter the NAME of table  ! "
-        read t6
-	echo "Please enter the NAME of feild like this 'feild_naame datatype (int,varchar(255),...)' ! "
-	read f6
-	mysql -uroot -p${rootpasswd} -e "ALTER TABLE ${db6}.${t6} ADD ${f6} ;" 2>/dev/null
-	mysql -uroot -p${rootpasswd} -e "DESCRIBE ${db6}.${t6}" 2>/dev/null
-	echo "Field  ${f6} successfully Created!"    ;;
+  5)    ls  $HOME/DB-managemnt/
+        echo "Please enter the NAME database you want to delete table on it ! "
+        read db
+        echo "--------------"
+        ls  $HOME/DB-managemnt/${db}/
+        echo "Please enter the NAME table ! "
+        read tb
+	echo "Please enter the all feild like c1,c2,c3,....... ! "
+        read f
+        echo "${f}" >> $HOME/DB-managemnt/${db}/${tb}
+        cat $HOME/DB-managemnt/${db}/${tb}
+	;;
+  6)  ls  $HOME/DB-managemnt/
+        echo "Please enter the NAME database you want to delete table on it ! "
+        read db
+        echo "--------------"
+        ls  $HOME/DB-managemnt/${db}/
+        echo "Please enter the NAME table ! "
+        read tb
+        echo "Please enter the number of feild ! "
+        read f
+	awk -F, '{for(i=1;i<=NF;i++)if(i!=x)f=f?f FS $i:$i;print f;f=""}' x=${f} $HOME/DB-managemnt/${db}/${tb} >> $HOME/DB-managemnt/${db}/${tb}.new
+	mv $HOME/DB-managemnt/${db}/${tb}.new $HOME/DB-managemnt/${db}/${tb}
+	colum -t  $HOME/DB-managemnt/${db}/${tb}
 
-  7) echo "Showing existing databases..."
-        mysql -uroot -p${rootpasswd} -e "show databases;"
-        echo ""
-        echo "Please enter the NAME database  ! "
-        read db7
-        mysql -uroot -p${rootpasswd} ${db7} -e  "show tables;" 2>/dev/null
-        echo ""
-        echo "Please enter the NAME of table  ! "
-        read t7
-        mysql -uroot -p${rootpasswd} -e "DESCRIBE ${db7}.${t7}" 2>/dev/null
-	echo ""
-	echo "Please enter colums names like c1,c2,c3,..."
-	read c7
-	echo "Please enter values of record like v1,v2,v3,... if string Please add between '' "
-	read v7
-	mysql -uroot -p${rootpasswd} -e  "INSERT INTO ${db7}.${t7}(${c7}) VALUES (${v7});" 2>/dev/null
-	mysql -uroot -p${rootpasswd} -e " SELECT * FROM  ${db7}.${t7};" 2>/dev/null
-	echo "" ;;
+    ;;
+
+  7) 
   8) echo "Showing existing databases..."
         mysql -uroot -p${rootpasswd} -e "show databases;"
         echo ""
